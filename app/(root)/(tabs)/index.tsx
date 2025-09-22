4a04c617090765be070bf6dd629ba00348b41f61
Samuel Abu <abu.sammie@gmail.com>
2025-09-22 15:32:05 +0200

    font usage fixed

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,16 +1,32 @@
+import { useFonts } from "expo-font";
 import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
+
 export default function Index() {
+  const [fontsLoaded] = useFonts({
+    "Rubik-Bold": require("../../../assets/fonts/Rubik-Bold.ttf"),
+    "Rubik-ExtraBold": require("../../../assets/fonts/Rubik-ExtraBold.ttf"),
+    "Rubik-Light": require("../../../assets/fonts/Rubik-Light.ttf"),
+    "Rubik-Medium": require("../../../assets/fonts/Rubik-Medium.ttf"),
+    "Rubik-Regular": require("../../../assets/fonts/Rubik-Regular.ttf"),
+    "Rubik-SemiBold": require("../../../assets/fonts/Rubik-SemiBold.ttf"),
+  });
+  
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <View
       style={{
         flex: 1,
         justifyContent: "center",
-        alignItems: "center",
+        alignItems: "center"
       }}
     >
-      <Text className="font-bold text-lg my-10">Welcome to ReState</Text>
+      
+      <Text style={{ fontFamily: 'Rubik-Regular', fontSize: 24}}>Welcome to Real Estate</Text> 
       <Link href='/sign-in'>Sign In</Link>
       <Link href='/explore'>Explore</Link>
       <Link href='/profile'>Profile</Link>
